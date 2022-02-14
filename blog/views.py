db08d6e049a520ac126c5f10f04ec0a03413de88
Sehyun Kim <36873797+sankim3@users.noreply.github.com>
2022-02-14 11:16:44 +0900

    Function based View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk') # private key, pk의 역순으로 정렬 -> 가장 최근에 만든 포스트부터 나열.
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+        'posts' : posts,
+        }
+    )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk = pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
