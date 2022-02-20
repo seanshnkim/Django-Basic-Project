e25828576115dd27bff92a8b5b6a91d7034cde00
sehyunkim <sehyun.seankim@gmail.com>
2022-02-20 21:02:30 +0900

    author field added

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -12,8 +13,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     # 포스트 상세 페이지를 만들 때 URL이 도메인 뒤에 /blog/pk of record/를 쓰도록 했기 때문
     def get_absolute_url(self):
